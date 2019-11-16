13d4e9bf8b46d9ee76825155b7e6cae0337c422d
Simon Han <hanfresco@gmail.com>
2019-11-16 02:38:59 -0800

    Moved junior parameters values to a properties file

--- java/Thyrosim.java
+++ java/Thyrosim.java
@@ -1,4 +1,9 @@
 package edu.ucla.distefanolab.thyrosim.algorithm;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
 import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 import org.apache.commons.math3.ode.FirstOrderIntegrator;
 import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
@@ -17,72 +22,91 @@ public class Thyrosim implements FirstOrderDifferentialEquations
     // Functions that Java ODE solver needs
     // Declare parameters
     public Thyrosim(double dial1, double dial2, double dial3, double dial4,
-               double inf1,  double inf4)
+                    double inf1,  double inf4,  String thysim)
     {
-        u1 = inf1;      // Infusion into plasma T4
-        u4 = inf4;      // Infusion into plasma T3
-        kdelay = 5d/8d; // (n-1)/k = t; n comps, t = 8hr
-        d1 = dial1;
+        u1 = inf1;  // Infusion into plasma T4
+        u4 = inf4;  // Infusion into plasma T3
+        d1 = dial1; // Dial values
         d2 = dial2;
         d3 = dial3;
         d4 = dial4;
 
-        p1 = 0.00174155;                // S4
-        p2 = 8;                         // tau
-        p3 = 0.868;                     // k12
-        p4 = 0.108;                     // k13
-        p5 = 584;                       // k31free
-        p6 = 1503;                      // k21free
-        p7 = 0.000289;                  // A
-        p8 = 0.000214;                  // B
-        p9 = 0.000128;                  // C
-        p10 = -8.83 * Math.pow(10,-6);  // D
-        p11 = 0.88;                     // k4absorb; originally 0.881
-        p12 = 0.0189;                   // k02
-        p13 = 0.00998996;               // VmaxD1fast
-        p14 = 2.85;                     // KmD1fast
-        p15 = 6.63  * Math.pow(10,-4);  // VmaxD1slow
-        p16 = 95;                       // KmD1slow
-        p17 = 0.00074619;               // VmaxD2slow
-        p18 = 0.075;                    // KmD2slow
-        p19 = 3.3572* Math.pow(10,-4);  // S3
-        p20 = 5.37;                     // k45
-        p21 = 0.0689;                   // k46
-        p22 = 127;                      // k64free
-        p23 = 2043;                     // k54free
-        p24 = 0.00395;                  // a
-        p25 = 0.00185;                  // b
-        p26 = 0.00061;                  // c
-        p27 = -0.000505;                // d
-        p28 = 0.88;                     // k3absorb; originally 0.882
-        p29 = 0.207;                    // k05
-        p30 = 1166;                     // Bzero
-        p31 = 581;                      // Azero
-        p32 = 2.37;                     // Amax
-        p33 = -3.71;                    // phi
-        p34 = 0.53;                     // kdegTSH-HYPO
-        p35 = 0.037;                    // VmaxTSH
-        p36 = 23;                       // K50TSH
-        p37 = 0.118;                    // k3
-        p38 = 0.29;                     // T4P-EU
-        p39 = 0.006;                    // T3P-EU
-        p40 = 0.037;                    // KdegT3B
-        p41 = 0.0034;                   // KLAG-HYPO
-        p42 = 5;                        // KLAG
-        p43 = 1.3;                      // k4dissolve
-        p44 = 0.12 * d2;                // k4excrete; originally 0.119
-        p45 = 1.78;                     // k3dissolve
-        p46 = 0.12 * d4;                // k3excrete; originally 0.118
-        // p47 and p48 are only used in converting mols to units. Since unit
-        // conversion is done in THYSIM->postProcess(), make sure you change
-        // p47 and p48 there if you need to change these values.
-        p47 = 3.2;                      // Vp
-        p48 = 5.2;                      // VTSH		   
+        // Load properties
+        Properties prop = new Properties();
+        String paramsfile = "../config/" + thysim + ".params";
+        InputStream pis = null;
+
+        try {
+            pis = new FileInputStream(paramsfile);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found: " + paramsfile);
+        }
+
+        try {
+            prop.load(pis);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+        // Convert properties to double
+        kdelay = Double.valueOf(prop.getProperty("kdelay"));
+        p1  = Double.valueOf(prop.getProperty("p1"));
+        p2  = Double.valueOf(prop.getProperty("p2"));
+        p3  = Double.valueOf(prop.getProperty("p3"));
+        p4  = Double.valueOf(prop.getProperty("p4"));
+        p5  = Double.valueOf(prop.getProperty("p5"));
+        p6  = Double.valueOf(prop.getProperty("p6"));
+        p7  = Double.valueOf(prop.getProperty("p7"));
+        p8  = Double.valueOf(prop.getProperty("p8"));
+        p9  = Double.valueOf(prop.getProperty("p9"));
+        p10 = Double.valueOf(prop.getProperty("p10"));
+        p11 = Double.valueOf(prop.getProperty("p11"));
+        p12 = Double.valueOf(prop.getProperty("p12"));
+        p13 = Double.valueOf(prop.getProperty("p13"));
+        p14 = Double.valueOf(prop.getProperty("p14"));
+        p15 = Double.valueOf(prop.getProperty("p15"));
+        p16 = Double.valueOf(prop.getProperty("p16"));
+        p17 = Double.valueOf(prop.getProperty("p17"));
+        p18 = Double.valueOf(prop.getProperty("p18"));
+        p19 = Double.valueOf(prop.getProperty("p19"));
+        p20 = Double.valueOf(prop.getProperty("p20"));
+        p21 = Double.valueOf(prop.getProperty("p21"));
+        p22 = Double.valueOf(prop.getProperty("p22"));
+        p23 = Double.valueOf(prop.getProperty("p23"));
+        p24 = Double.valueOf(prop.getProperty("p24"));
+        p25 = Double.valueOf(prop.getProperty("p25"));
+        p26 = Double.valueOf(prop.getProperty("p26"));
+        p27 = Double.valueOf(prop.getProperty("p27"));
+        p28 = Double.valueOf(prop.getProperty("p28"));
+        p29 = Double.valueOf(prop.getProperty("p29"));
+        p30 = Double.valueOf(prop.getProperty("p30"));
+        p31 = Double.valueOf(prop.getProperty("p31"));
+        p32 = Double.valueOf(prop.getProperty("p32"));
+        p33 = Double.valueOf(prop.getProperty("p33"));
+        p34 = Double.valueOf(prop.getProperty("p34"));
+        p35 = Double.valueOf(prop.getProperty("p35"));
+        p36 = Double.valueOf(prop.getProperty("p36"));
+        p37 = Double.valueOf(prop.getProperty("p37"));
+        p38 = Double.valueOf(prop.getProperty("p38"));
+        p39 = Double.valueOf(prop.getProperty("p39"));
+        p40 = Double.valueOf(prop.getProperty("p40"));
+        p41 = Double.valueOf(prop.getProperty("p41"));
+        p42 = Double.valueOf(prop.getProperty("p42"));
+        p43 = Double.valueOf(prop.getProperty("p43"));
+        p44 = Double.valueOf(prop.getProperty("p44"));
+        p45 = Double.valueOf(prop.getProperty("p45"));
+        p46 = Double.valueOf(prop.getProperty("p46"));
+        p47 = Double.valueOf(prop.getProperty("p47"));
+        p48 = Double.valueOf(prop.getProperty("p48"));
+
+        // Post prop load modification
+        p44 = p44 * d2;
+        p46 = p46 * d4;
     }
 
     public int getDimension()
     {
-        return 19;
+        return 21;
     }
 
     public void computeDerivatives(double t, double[] q, double[] qDot)
@@ -123,6 +147,10 @@ qDot[15] = kdelay*(q[14] -q[15]);                               // delay3
 qDot[16] = kdelay*(q[15] -q[16]);                               // delay4
 qDot[17] = kdelay*(q[16] -q[17]);                               // delay5
 qDot[18] = kdelay*(q[17] -q[18]);                               // delay6
+
+// Additional values
+qDot[19] = q1F; // FT4p
+qDot[20] = q4F; // FT3p
     }
 
     public static void main(String[] args)
@@ -154,12 +182,16 @@ qDot[18] = kdelay*(q[17] -q[18]);                               // delay6
         double dial4 = Double.parseDouble(args[24]);
         double inf1  = Double.parseDouble(args[25]);
         double inf4  = Double.parseDouble(args[26]);
+        String thysim = args[27];
 
         FirstOrderIntegrator integrator = new DormandPrince853Integrator(1.0e-8,100.0,1.0e-10,1.0e-10);
         // FirstOrderIntegrator integrator = new GraggBulirschStoerIntegrator(1.0e-8, 100.0, 1.0e-10, 1.0e-10);
 
-        double[] q = new double[] {IC1,IC2,IC3,IC4,IC5,IC6,IC7,IC8,IC9,IC10,IC11,IC12,IC13,IC14,IC15,IC16,IC17,IC18,IC19}; // Initial state
-        Thyrosim ode = new Thyrosim(dial1, dial2, dial3, dial4, inf1, inf4);
+        // Initial state
+        double[] q = new double[] {IC1, IC2, IC3, IC4, IC5, IC6,
+                                   IC7, IC8, IC9, IC10,IC11,IC12,
+                                   IC13,IC14,IC15,IC16,IC17,IC18,IC19};
+        Thyrosim ode = new Thyrosim(dial1,dial2,dial3,dial4,inf1,inf4,thysim);
 
         int t1 = (int)Math.round(t1d);
         int t2 = (int)Math.round(t2d);
@@ -184,9 +216,9 @@ qDot[18] = kdelay*(q[17] -q[18]);                               // delay6
         double[] q17 = new double[t2 - t1 + 1];
         double[] q18 = new double[t2 - t1 + 1];
         double[] q19 = new double[t2 - t1 + 1];
+        double[] q20 = new double[t2 - t1 + 1]; // FT4p values
+        double[] q21 = new double[t2 - t1 + 1]; // FT3p values
         double[] ts  = new double[t2 - t1 + 1];
-        double[] ft4 = new double[t2 - t1 + 1]; // FT4p values
-        double[] ft3 = new double[t2 - t1 + 1]; // FT3p values
         for (int i = t1; i <= t2; i++) {
             q1[i]  = q[0];
             q2[i]  = q[1];
@@ -207,11 +239,9 @@ qDot[18] = kdelay*(q[17] -q[18]);                               // delay6
             q17[i] = q[16];
             q18[i] = q[17];
             q19[i] = q[18];
+            q20[i] = q[19];
+            q21[i] = q[20];
             ts[i]  = i;
-
-            // Copy/paste equations from computeDerivatives
-            ft4[i] = (ode.p7 +ode.p8 *q[0]+ode.p9 *Math.pow(q[0],2)+ode.p10*Math.pow(q[0],3))*q[0]; // FT4p
-            ft3[i] = (ode.p24+ode.p25*q[0]+ode.p26*Math.pow(q[0],2)+ode.p27*Math.pow(q[0],3))*q[3]; // FT3p
 
             integrator.integrate(ode,i,q,i+1,q);
         }
@@ -292,17 +322,17 @@ qDot[18] = kdelay*(q[17] -q[18]);                               // delay6
         printArray(q19);
         System.out.println("END_q19_END");
 
+        System.out.println("START_q20_START");
+        printArray(q20);
+        System.out.println("END_q20_END");
+
+        System.out.println("START_q21_START");
+        printArray(q21);
+        System.out.println("END_q21_END");
+
         System.out.println("START_t_START");
         printArray(ts);
         System.out.println("END_t_END");
-
-        System.out.println("START_ft4_START");
-        printArray(ft4);
-        System.out.println("END_ft4_END");
-
-        System.out.println("START_ft3_START");
-        printArray(ft3);
-        System.out.println("END_ft3_END");
 
     }
 
